5ba23606a162820bebff1f20d7c4fb58b01c06a2
mahm0ud-hamed <mahmoudmohamedhmd@gmail.com>
2024-07-15 12:08:16 +0300

    2d vector transpose

--- cpplabs/modernlabs/Vectorlab/vectorlab.cpp
+++ cpplabs/modernlabs/Vectorlab/vectorlab.cpp
@@ -4,20 +4,39 @@
 std::vector<std::vector<int>> SwapRowColoumn(std::vector<std::vector<int>> &myVec) ;
 
 int main(){
-std::vector<std::vector<int>> myvec(3 , 3 )= {{1,2,3},
-                                      {4,5,6},
-                                      {7,8,9}};
-
+std::vector<std::vector<int>> myvec= {
+    {1,2},
+    {4,5},
+    {7,8}
+};
+/*for(const auto  &out : myvec){
+    for(auto in : out){
+        std::cout<< in <<" "; 
+    }
+    std::cout<< std::endl ; 
+}*/
+std::vector<std::vector<int>> newvec = SwapRowColoumn(myvec) ;
+for(const auto  &out : newvec){
+    for(auto in : out){
+        std::cout<< in <<" "; 
+    }
+    std::cout<< std::endl ; 
+}
 }
 
 
 std::vector<std::vector<int>> SwapRowColoumn(std::vector<std::vector<int>> &myVec){
-    std::vector<std::vector<int>> tempVec; 
-    int i=0  , j=0  ; 
-    for ( i=0  ; i < myVec[i].end(); i++){
-        for ( j=0 ; j< myVec.end; j++){
-
+ 
+    int colm= myVec[0].size();
+    int rows = myVec.size() ; 
+    // allocte new vector with 
+    std::vector<std::vector<int>> tempVec(colm , std::vector<int>(rows));
+    std::cout<<"cols " << colm << " rwos "<<rows<<std::endl; 
+    for(int i=0 ; i<rows ; i++){
+        for(int j = 0 ; j < colm ; j++){
+            tempVec[j][i] = myVec[i][j] ; 
         }
     }
+    return tempVec ; 
 
 }
